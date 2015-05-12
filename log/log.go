875f2bcbf9a7881a3bb7caab605b369b60d72ed0
oursky d <gogames@users.noreply.github.com>
2015-05-12 15:38:05 +0800

    refs #3, enhance back trace information for better debug

--- log/log.go
+++ log/log.go
@@ -18,16 +18,26 @@ var (
 	Error = log.New(os.Stdout, "[E] ", flag)
 	Fatal = log.New(os.Stderr, "[F] ", flag)
 )
+
 var gopath = os.Getenv("GOPATH") + "/src/"
+var goroot = os.Getenv("GOROOT") + "/src/"
 
 // Fatalf mirror log Fatalf
-func Fatalf(format string, args ...interface{}) { Fatal.Fatalf(getCallerInfo()+format, args...) }
+func Fatalf(format string, args ...interface{}) { Fatal.Fatalf(backTrace()+format+"\n", args...) }
 
-func getCallerInfo() string {
-	b := bytes.NewBuffer(nil)
-	_, file, line, ok := runtime.Caller(2)
-	if ok {
-		b.WriteString(strings.TrimPrefix(file, gopath) + "#" + strconv.Itoa(line) + " ~ ")
+func backTrace() string {
+	body := bytes.NewBufferString("\n")
+
+	for skip := 2; ; skip++ {
+		_, file, line, ok := runtime.Caller(skip)
+		if !ok {
+			break
+		}
+		if file[len(file)-1] == 'c' {
+			continue
+		}
+		body.WriteString("\t" + strings.TrimPrefix(strings.TrimPrefix(file, gopath), goroot) + "#" + strconv.Itoa(line) + "\n")
 	}
-	return b.String()
+
+	return body.String()
 }
